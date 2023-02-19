24ad04854e03b50de94c0a6ed27e2ebb681022bd
NIckel <44306856+nickelsmirnoff@users.noreply.github.com>
2023-02-19 15:54:37 +0300

    1

---
+++ readme.txt
@@ -0,0 +1,4 @@
+This script converts any roman number(less than 4000)
+to decimal number.
+input is string with roman number
+output is decimal value of this number
---
+++ roman_converter.py
@@ -0,0 +1,30 @@
+str_input = input()
+
+def roman(string):
+    '''This function converts roman number less than 4000(!)
+    to arabic number.
+    input is string with roman number
+    returns decimal value of this roman number'''
+
+    unnorm_roman = {'IV': '4', 'IX': '9', 'XL': '40', 'XC': '90', 'CD': '400', 'CM': '900'}
+    normal_roman = {'I': '1', 'V': '5', 'X': '10', 'L': '50', 'C': '100', 'D': '500', 'M': '1000'}
+    result = 0
+    str_arr = list(string)
+    if len(string) > 1:
+        for i in range(len(str_arr)-1):
+            char = str_arr[i]+str_arr[i+1]
+            if char in unnorm_roman.keys():
+                result += int(unnorm_roman[char])
+                i += 1
+    new_str = string.replace('IV','')
+    new_str = new_str.replace('IX','')
+    new_str = new_str.replace('XL','')
+    new_str = new_str.replace('XC','')
+    new_str = new_str.replace('CD','')
+    new_str = new_str.replace('CM','')
+
+    for c in new_str:
+        result += int(normal_roman[c])
+    return result
+
+print(roman(str_input))
